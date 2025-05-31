c7e4ed609f79c9a3673502523c70ec515ccc1527
Anton Kuznetsov <anton.kuznetsov.mail@gmail.com>
2025-05-31 12:44:29 +0200

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Anton Kuznetsov on 31.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
