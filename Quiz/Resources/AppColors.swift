ca1bef44b7a5950d91604628f9690357990f047f
Anton Kuznetsov <anton.kuznetsov.mail@gmail.com>
2025-05-31 13:21:24 +0200

    Fonts, images, localizable, Firebase and TCA

---
+++ Quiz/Resources/AppColors.swift
@@ -0,0 +1,19 @@
+//
+//  AppColors.swift
+//  Quiz
+//
+//  Created by Anton Kuznetsov on 31.05.2025.
+//
+
+import SwiftUI
+
+enum AppColors {
+    
+    // MARK: - Buttons
+    static let primaryButtonBackgroundColor = Color(red: 24/255, green: 24/255, blue: 25/255, opacity: 1)
+    static let darkModeButtonBackgroundColor = Color(red: 255, green: 255, blue: 255, opacity: 1)
+    
+    // MARK: - Text
+    static let primaryTextColor = Color(red: 24/255, green: 24/255, blue: 25/255, opacity: 1)
+    static let darkModeTextColor = Color(red: 255, green: 255, blue: 255, opacity: 1)
+}
---
+++ Quiz/Resources/AppImages.swift
@@ -0,0 +1,13 @@
+//
+//  AppImages.swift
+//  Quiz
+//
+//  Created by Anton Kuznetsov on 31.05.2025.
+//
+
+import SwiftUI
+
+enum Images {
+    // I've decided to make the main screen's background image static, but of course we can parse it from the server if needed
+    static let welcomeBackground = Image("main_background")
+}
